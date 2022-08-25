b181a758a0d1a7904a8a9be8c4f7f5e6c77441b2
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-08-25 15:55:08 +0900

    💄 design: TheHeader 스타일 및 레이아웃 추가/#1

--- src/components/layout/TheHeader/index.jsx
+++ src/components/layout/TheHeader/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import * as S from './style';
+import {
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+  AiOutlineMenu,
+} from 'react-icons/ai';
 
 const TheHeader = () => {
-  return <div></div>;
+  return (
+    <S.TheHeader>
+      {AiOutlineSearch()}
+      {AiOutlineShoppingCart()}
+      {AiOutlineMenu()}
+    </S.TheHeader>
+  );
 };
 
 export default TheHeader;
