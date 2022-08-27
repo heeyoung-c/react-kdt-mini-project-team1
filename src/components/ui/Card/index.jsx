ac94ba1e6cf1ff0f7dc4827f416a86e7bfe1b2c6
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-08-27 13:15:13 +0900

    ✨ feat: 장바구니, 찜하기 버튼 추가/#17

--- src/components/ui/Card/index.jsx
+++ src/components/ui/Card/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
-import Button from '../TheButton';
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineShoppingCart,
+} from 'react-icons/ai';
 
 const Card = ({ title, supporter, amount }) => {
+  const [isHearted, setIsHearted] = useState(false);
+
   return (
     <S.Container>
       <S.Card>
@@ -13,14 +19,20 @@ const Card = ({ title, supporter, amount }) => {
           </div>
         </S.CardTop>
         <S.CardBottom>
-          <S.FlexGlow />
-          <S.AmoutDiv>
-            <S.AmoutTitle>한도</S.AmoutTitle> {amount} 원
-          </S.AmoutDiv>
+          <S.FlexGrow />
+          <S.AmountDiv>
+            <S.AmountTitle>한도</S.AmountTitle> {amount} 원
+          </S.AmountDiv>
         </S.CardBottom>
         <S.ButtonDiv>
-          <S.FlexGlow />
-          <Button buttonName={'신청'} />
+          <S.FlexGrow />
+          <S.Heart
+            onClick={() => setIsHearted(!isHearted)}
+            isHearted={isHearted}
+          >
+            {isHearted ? AiFillHeart() : AiOutlineHeart()}
+          </S.Heart>
+          <div>{AiOutlineShoppingCart()}</div>
         </S.ButtonDiv>
       </S.Card>
     </S.Container>
