c9abf52d666b735b0dd43fa94b372f878389cfb4
hoona1011 <jasonpark90@naver.com>
2022-08-30 21:32:26 +0900

    ✨ feat:: 회원가입 페이지 맞춤형 정보 입력란 구현/#32

--- src/components/ui/TheButton/index.jsx
+++ src/components/ui/TheButton/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import * as S from './style';
 
-const TheButton = ({ buttonName, onClick }) => {
-  return <S.TheButton onClick={onClick}>{buttonName}</S.TheButton>;
+const TheButton = ({ buttonName, onClick, formbutton }) => {
+  return (
+    <S.TheButton onClick={onClick} formbutton={formbutton}>
+      {buttonName}
+    </S.TheButton>
+  );
 };
 
 export default TheButton;
