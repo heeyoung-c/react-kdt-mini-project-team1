1f2c657d1abfec8ab514a8e81d02f83964d13849
kyle-shk <guqtls85@naver.com>
2022-09-01 02:22:18 +0900

    🔧 config: eslint 오류 해결/#44

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { ThemeProvider } from 'styled-components';
 import AppRouter from './routes/AppRouter';
 import GlobalStyle from './styles/globalStyle';
 import theme from './styles/theme';
-
 import TheHeader from './components/layout/TheHeader';
 import TheFooter from './components/layout/TheFooter';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 // 화살표 함수
 const App = () => {
   return (
