b4b32b0954ec3f9126c67bf4ee0ab3b22b13c38c
kyle-shk <guqtls85@naver.com>
2022-09-01 02:40:08 +0900

    ✨ feat: usercard 컴포넌트에서 user api 출력/#57

--- src/components/ui/UserCard/index.jsx
+++ src/components/ui/UserCard/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import * as S from './style';
+import { useGetPersonQuery } from '~/api/customApi';
 function UserCard() {
+  const { data: person, isLoading, isError } = useGetPersonQuery();
+  if (isLoading) {
+    return <div>로딩 중...</div>;
+  }
+  if (isError || !person) {
+    return <div>오류 발생</div>;
+  }
   return (
     <S.Container>
       <S.Card>
-        <h1>김준태 님의 정보를 바탕으로 분석한</h1>
+        <h1>{person.username}님의 정보를 바탕으로 분석한</h1>
         <br />
         <h1>현재 신청 가능 대출</h1>
       </S.Card>
