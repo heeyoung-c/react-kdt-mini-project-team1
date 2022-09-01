6a57d982ceef4f82b9d49ba7471961360d785a40
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-09-01 11:41:17 +0900

    ✨ feat: RTK 쿼리를 사용한 Api 통신 추가/#20

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -1,26 +1,117 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const url = import.meta.env.VITE_SERVICE_URL;
+const accessToken =
+  'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLsp4Dtm4giLCJpZCI6NSwiZXhwIjoxNjYyODA1NjE5LCJlbWFpbCI6IndsZ25zQG5hdmVyLmNvbSJ9.3AS4Xmm_UNN-4lDp-X6qGKq7VQUAVrdmElA8ncb_VoqIjOx3eE60rqzbYhaDAjCSwgIK3if9uzXgi1hkacoyKw';
 
 export const productsApi = createApi({
   reducerPath: 'productsApi',
+  tagTypes: ['Products'],
   baseQuery: fetchBaseQuery({
     baseUrl: url,
   }),
   endpoints: builder => ({
-    getWishProducts: builder.query({
+    // 전체 상품 조회
+    getAllProducts: builder.query({
       query: () => ({
         url: 'products/list',
         method: 'GET',
         headers: {
-          Authorization:
-            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZWFtMSIsImlkIjo0LCJleHAiOjE2NjI4MTA5NjMsImVtYWlsIjoidGVhbTFAZ21haWwuY29tIn0.sOBnKBtk1rFguwsVDFYiWcDmNLXWKMZGHLuLGZFGNwYD0DOSqaXHoLEz504MozfBg_M4oKKgJMB3vLvj58ozIw',
+          Authorization: accessToken,
+        },
+      }),
+      providesTags: [{ type: 'Products', id: 'ALLLIST' }],
+      transformResponse: responseData => {
+        return responseData['data'];
+      },
+    }),
+
+    // 찜 상품 조회
+    getWishProducts: builder.query({
+      query: () => ({
+        url: 'bookmarks/find',
+        method: 'GET',
+        headers: {
+          Authorization: accessToken,
         },
       }),
       transformResponse: responseData => {
         return responseData['data'];
       },
     }),
+
+    // 찜 상품 등록
+    addBookMark: builder.mutation({
+      query: payload => ({
+        url: 'bookmarks/add',
+        method: 'POST',
+        body: payload,
+        headers: {
+          Authorization: accessToken,
+        },
+      }),
+      invalidatesTags: result =>
+        result ? [{ type: 'Products', id: 'ALLLIST' }] : [],
+    }),
+
+    // 찜 상품 삭제
+    deleteBookMark: builder.mutation({
+      query: id => ({
+        url: `bookmarks/${id}`,
+        method: 'DELETE',
+        headers: {
+          Authorization: accessToken,
+        },
+      }),
+    }),
+
+    // 장바구니 상품 조회
+    getCartsProducts: builder.query({
+      query: () => ({
+        url: 'carts/find',
+        method: 'GET',
+        headers: {
+          Authorization: accessToken,
+        },
+      }),
+      providesTags: [{ type: 'Products', id: 'CARTSLIST' }],
+      transformResponse: responseData => {
+        return responseData['data'];
+      },
+    }),
+
+    // 장바구니 상품 등록
+    addCartsProducts: builder.mutation({
+      query: payload => ({
+        url: 'carts/add',
+        method: 'POST',
+        body: payload,
+        headers: {
+          Authorization: accessToken,
+        },
+      }),
+      invalidatesTags: result =>
+        result ? [{ type: 'Products', id: 'CARTSLIST' }] : [],
+    }),
+
+    // 장바구니 상품 단일 삭제
+    deleteCartProduct: builder.mutation({
+      query: id => ({
+        url: `carts/${id}`,
+        method: 'DELETE',
+        headers: {
+          Authorization: accessToken,
+        },
+      }),
+    }),
   }),
 });
 
-export const { useGetWishProductsQuery } = productsApi;
+export const {
+  useGetAllProductsQuery,
+  useGetWishProductsQuery,
+  useAddBookMarkMutation,
+  useDeleteBookMarkMutation,
+  useGetCartsProductsQuery,
+  useAddCartsProductsMutation,
+  useDeleteCartProductMutation,
+} = productsApi;
