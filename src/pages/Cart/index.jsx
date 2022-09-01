3e276cea84cd47f698253b1a3eda5910f35647de
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-09-01 11:43:24 +0900

    ✨ feat: 기본적인 carts 작업/#20

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import * as S from './style';
+import Card from '~/components/ui/Card';
+import { useGetCartsProductsQuery } from '~/api/productsApi';
 
 const Cart = () => {
-  return (
-    <S.Container>
-      <div></div>
-    </S.Container>
-  );
+  const {
+    data: cartsProducts,
+    isLoading,
+    isError,
+  } = useGetCartsProductsQuery();
+  if (isLoading) {
+    return <div>로딩 중...</div>;
+  }
+  if (isError || !cartsProducts) {
+    return <div>오류 발생</div>;
+  }
+  console.log(cartsProducts);
+
+  // 카트 목록이 있는 경우를, cartsProducts 첫번째 아이템에 id 값이 있는지로 판별
+  if (cartsProducts[0].id) {
+    return (
+      <>
+        {cartsProducts.map(product => {
+          const { id, supporterName, productName, supporterAmount } = product;
+
+          return (
+            <Card
+              key={id}
+              title={productName}
+              supporter={supporterName}
+              amount={supporterAmount
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              bookmark={false}
+              id={id}
+              renderType='Cart'
+            />
+          );
+        })}
+      </>
+    );
+  } else {
+    return <p>찜 목록이 없습니다</p>;
+  }
 };
 
 export default Cart;
--- src/pages/Cart/style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  border: 0.1rem solid;
-  height: 50vh;
-`;
