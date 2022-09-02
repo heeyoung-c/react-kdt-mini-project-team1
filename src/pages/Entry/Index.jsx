98b3bf5057641d82297f225516ad271e0a6ad4d9
hoona1011 <jasonpark90@naver.com>
2022-09-02 16:31:08 +0900

    ✨ feat: 로그인 여부에 따라 진입페이지로 리다이렉트/#69

---
+++ src/pages/Entry/Index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import * as S from './style';
+
+const Entry = () => {
+  const navigate = useNavigate();
+  return (
+    <S.Container>
+      <S.Title>서비스 페이지입니다</S.Title>
+      <S.Button onClick={() => navigate('/sign-in')}>로그인</S.Button>
+      <S.Button onClick={() => navigate('/sign-up')}>회원가입</S.Button>
+    </S.Container>
+  );
+};
+
+export default Entry;
