de9be750331634d8bc66a0b1c04e27eeb3a45695
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-09-02 23:36:21 +0900

    ✨ feat: 장바구니에 담긴 상품의 숫자 표시 작업/#74

--- src/components/layout/TheHeader/index.jsx
+++ src/components/layout/TheHeader/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import * as S from './style';
 
-import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { BiArrowBack } from 'react-icons/bi';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import BasicMenu from '../../Modal/hamburger';
+import { useGetCartsProductsQuery } from '../../../api/productsApi';
+
 const TheHeader = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const { data: cartProducts, isLoading, isError } = useGetCartsProductsQuery();
 
   return (
     <S.TheHeader>
@@ -17,6 +20,12 @@ const TheHeader = () => {
         <S.ArrowBack onClick={() => navigate('/')}>{BiArrowBack()}</S.ArrowBack>
       ) : null}
       <div onClick={() => navigate('/cart')}>{AiOutlineShoppingCart()}</div>
+
+      {/* cartProducts의 타입이 'string인 경우는 장바구니에 상품이 없는 경우 이기 때문에 
+      이와 같이 코드를 작성하였습니다*/}
+      {typeof cartProducts === 'string' ? null : (
+        <S.CartLength>{cartProducts && cartProducts.length}</S.CartLength>
+      )}
 
       <BasicMenu />
     </S.TheHeader>
--- src/components/layout/TheHeader/style.js
+++ src/components/layout/TheHeader/style.js
@@ -10,3 +10,16 @@ export const ArrowBack = styled.div`
   position: absolute;
   left: 1rem;
 `;
+
+export const CartLength = styled.div`
+  position: absolute;
+  top: 0.8rem;
+  right: 5.5rem;
+  ${({ theme }) => theme.common.flexCenter};
+  width: 1.5rem;
+  height: 1.5rem;
+  background-color: ${({ theme }) => theme.palette.red};
+  color: ${({ theme }) => theme.palette.white};
+  font-size: ${({ theme }) => theme.fontSizes.smallParagraph};
+  border-radius: 1rem;
+`;
