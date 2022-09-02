c85f6c1fe188689980b2b5a371a46d200345a996
SongE12 <aassdd6434@naver.com>
2022-09-02 21:25:03 +0900

    💄 design: header 디자인 수정#84

--- src/components/layout/TheHeader/style.js
+++ src/components/layout/TheHeader/style.js
@@ -1,12 +1,47 @@
+import { style } from '@mui/system';
 import styled from 'styled-components';
 
 export const TheHeader = styled.header`
+  font-size: ${({ theme }) => theme.fontSizes.title};
+`;
+
+export const Header_1 = styled.header`
   ${({ theme }) => theme.common.flexEnd}
-  height: 4rem;
+  margin-bottom: 0.3rem
+`;
+
+export const Battery = styled.div`
+  font-size: 2.1rem;
+  transform: rotate(-90deg);
+  margin-right: 2.3rem;
+`;
+
+export const Wifi = styled.div`
+  font-size: 1.8rem;
+  margin-right: 0.5rem;
+`;
+
+export const Signal = styled.div`
+  font-size: 1.8rem;
+  margin-right: 0.3rem;
+`;
+
+export const Time = styled.div`
+  font-weight: 700;
+  font-size: 1.5rem;
+  margin-left: 2.3rem;
+`;
+
+export const Header_2 = styled.header`
+  ${({ theme }) => theme.common.flexEnd}
   font-size: ${({ theme }) => theme.fontSizes.title};
 `;
 
 export const ArrowBack = styled.div`
   position: absolute;
   left: 1rem;
+`;
+
+export const FlexGrow = styled.div`
+  flex-grow: 1;
 `;
